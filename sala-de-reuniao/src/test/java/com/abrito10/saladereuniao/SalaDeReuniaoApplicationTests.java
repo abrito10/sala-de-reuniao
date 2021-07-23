b054efaf9d6d29a5b9a6a3fc01333427693facb5
Adilson de Brito <abrito10@gmail.com>
2021-07-23 11:54:25 -0300

    first commit

---
+++ sala-de-reuniao/src/test/java/com/abrito10/saladereuniao/SalaDeReuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abrito10.saladereuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaDeReuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
